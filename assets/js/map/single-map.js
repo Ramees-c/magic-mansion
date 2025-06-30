8a270d07039a2f3047aebdf9af447e02a7b36ddf
Ramees <rameesc985@gmail.com>
2025-06-30 12:14:11 +0530

    first commit

---
+++ assets/js/map/single-map.js
@@ -0,0 +1,181 @@
+var mapStyles = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
+
+$.ajaxSetup({
+    cache: true
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Google Map - Homepage
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function createHomepageGoogleMap(_latitude,_longitude, _zoom){
+    /* setMapHeight(); */
+    if( document.getElementById('map') != null ){
+        $.getScript("assets/js/map/single-locations.js", function(){
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: _zoom,
+                scrollwheel: false,
+                center: new google.maps.LatLng(_latitude, _longitude),
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                styles: mapStyles
+            });
+            var i;
+            var newMarkers = [];
+            for (i = 0; i < locations.length; i++) {
+                var pictureLabel = document.createElement("img");
+                pictureLabel.src = locations[i][7];
+                var boxText = document.createElement("div");
+                var marker = new MarkerWithLabel({
+                    title: locations[i][1],
+                    position: new google.maps.LatLng(_latitude, _longitude),
+                    map: map,
+                    icon: 'assets/images/map/marker.png',
+                    labelContent: pictureLabel,
+                    labelAnchor: new google.maps.Point(50, 0),
+                    labelClass: "marker-style"
+                });
+                newMarkers.push(marker);
+                //Define the infobox
+                google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                    return function() {
+                        for (h = 0; h < newMarkers.length; h++) {
+                            newMarkers[h].infobox.close();
+                        }
+                        newMarkers[i].infobox.open(map, this);
+                    }
+                })(marker, i));
+
+            }
+            var clusterStyles = [
+                {
+                    url: 'assets/images/map/marker.png',
+                    height: 50,
+                    width: 50
+                }
+            ];
+            var markerCluster = new MarkerClusterer(map, newMarkers, {styles: clusterStyles, maxZoom: 15});
+            $('body').addClass('loaded');
+            setTimeout(function() {
+                $('body').removeClass('has-fullscreen-map');
+            }, 1000);
+            $('#map').removeClass('fade-map');
+
+            //  Dynamically show/hide markers --------------------------------------------------------------
+
+            google.maps.event.addListener(map, 'idle', function() {
+
+                for (var i=0; i < locations.length; i++) {
+                    if ( map.getBounds().contains(newMarkers[i].getPosition()) ){
+                        // newMarkers[i].setVisible(true); // <- Uncomment this line to use dynamic displaying of markers
+
+                        //newMarkers[i].setMap(map);
+                        //markerCluster.setMap(map);
+                    } else {
+                        // newMarkers[i].setVisible(false); // <- Uncomment this line to use dynamic displaying of markers
+
+                        //newMarkers[i].setMap(null);
+                        //markerCluster.setMap(null);
+                    }
+                }
+            });
+
+            // Function which set marker to the user position
+            function success(position) {
+                var center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                map.panTo(center);
+                $('#map').removeClass('fade-map');
+            }
+
+        });
+        // Enable Geo Location on button click
+        $('.geo-location').on("click", function() {
+            if (navigator.geolocation) {
+                $('#map').addClass('fade-map');
+                navigator.geolocation.getCurrentPosition(success);
+            } else {
+                error('Geo Location is not supported');
+            }
+        });
+    }
+}
+
+
